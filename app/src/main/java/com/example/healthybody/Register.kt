3980981e3ef64f531e438bdc7f8b5db317af3a24
MarieData <marie.fb1245@gmail.com>
2019-10-29 01:07:47 +0700

    refactor(Mobile):เพิ่มการกดปุ่มไปหน้า Registerinformation ได้เเล้ว

--- app/src/main/java/com/example/healthybody/Register.kt
+++ app/src/main/java/com/example/healthybody/Register.kt
@@ -1,12 +1,22 @@
 package com.example.healthybody
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_register.*
 
 class Register : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val button1 = findViewById(R.id.button1) as Button
+        button1.setOnClickListener {
+            val intent = Intent(this, Registerinformation::class.java)
+            startActivity(intent)
+
+        }
     }
 }
